0b8020bde4f139a53d4256a705b2e933bfbc3c79
Ben Stern <bstern@fortian.com>
2017-07-11 23:32:16 -0400

    Import from mrdave

--- netcam_rtsp.h
+++ netcam_rtsp.h
@@ -1,23 +1,35 @@
 #include "netcam.h"
+#ifdef HAVE_FFMPEG
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
 #include <libavutil/avutil.h>
 #include <libavutil/imgutils.h>
-
+#include <libswscale/swscale.h>
+#endif
 
 struct rtsp_context {
-	AVFormatContext*      format_context;
-	AVCodecContext*       codec_context;
-	int                   video_stream_index;
-	char*                 path;
-	char*                 user;
-	char*                 pass;
+#ifdef HAVE_FFMPEG
+    AVFormatContext *format_context;
+    AVCodecContext *codec_context;
+    AVFrame *frame;
+    AVFrame *swsframe_in;
+    AVFrame *swsframe_out;
+    struct SwsContext *swsctx;
+#endif
+    int swsframe_size;
+    int video_stream_index;
+    char *path;
+    char *user;
+    char *pass;
+    int readingframe;
+    int status;
+    struct timeval startreadtime;
 };
 
-int netcam_setup_rtsp(netcam_context_ptr netcam, struct url_t *url);
 struct rtsp_context *rtsp_new_context(void);
 void netcam_shutdown_rtsp(netcam_context_ptr netcam);
-int rtsp_connect(netcam_context_ptr netcam);
+int netcam_connect_rtsp(netcam_context_ptr netcam);
 int netcam_read_rtsp_image(netcam_context_ptr netcam);
-void netcam_reconnect_rtsp(netcam_context_ptr netcam);
+int netcam_setup_rtsp(netcam_context_ptr netcam, struct url_t *url);
+int netcam_next_rtsp(unsigned char *image , netcam_context_ptr netcam);
